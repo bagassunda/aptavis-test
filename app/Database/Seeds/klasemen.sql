8cad53a77225957c03622257371ed6fae2377664
bagassunda <bgsprg@gmail.com>
2024-02-21 19:02:33 +0700

    Update klasemen.sql

--- app/Database/Seeds/klasemen.sql
+++ app/Database/Seeds/klasemen.sql
@@ -1,13 +1,13 @@
 CREATE TABLE clubs (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    points INT DEFAULT 0, -- Kolom baru untuk menyimpan jumlah poin
-    matches_played INT DEFAULT 0, -- Kolom baru untuk menyimpan jumlah pertandingan yang dimainkan
-    wins INT DEFAULT 0, -- Kolom baru untuk menyimpan jumlah kemenangan
-    draws INT DEFAULT 0, -- Kolom baru untuk menyimpan jumlah seri
-    losses INT DEFAULT 0, -- Kolom baru untuk menyimpan jumlah kekalahan
-    goals_for INT DEFAULT 0, -- Kolom baru untuk menyimpan jumlah gol yang dicetak
-    goals_against INT DEFAULT 0 -- Kolom baru untuk menyimpan jumlah gol yang kebobolan
+    points INT DEFAULT 0,
+    matches_played INT DEFAULT 0, 
+    wins INT DEFAULT 0,
+    draws INT DEFAULT 0,
+    losses INT DEFAULT 0,
+    goals_for INT DEFAULT 0,
+    goals_against INT DEFAULT 0
 );
 
 CREATE TABLE matches (
