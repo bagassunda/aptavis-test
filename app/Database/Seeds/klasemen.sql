e6085529108d400623aa31a7cce59d17ebf65793
bagassunda <bgsprg@gmail.com>
2024-02-21 19:01:59 +0700

    Update klasemen.sql

--- app/Database/Seeds/klasemen.sql
+++ app/Database/Seeds/klasemen.sql
@@ -1,25 +1,21 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               8.0.30 - MySQL Community Server - GPL
--- Server OS:                    Win64
--- HeidiSQL Version:             12.1.0.6537
--- --------------------------------------------------------
+CREATE TABLE clubs (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    points INT DEFAULT 0, -- Kolom baru untuk menyimpan jumlah poin
+    matches_played INT DEFAULT 0, -- Kolom baru untuk menyimpan jumlah pertandingan yang dimainkan
+    wins INT DEFAULT 0, -- Kolom baru untuk menyimpan jumlah kemenangan
+    draws INT DEFAULT 0, -- Kolom baru untuk menyimpan jumlah seri
+    losses INT DEFAULT 0, -- Kolom baru untuk menyimpan jumlah kekalahan
+    goals_for INT DEFAULT 0, -- Kolom baru untuk menyimpan jumlah gol yang dicetak
+    goals_against INT DEFAULT 0 -- Kolom baru untuk menyimpan jumlah gol yang kebobolan
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+CREATE TABLE matches (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    home_team_id INT,
+    away_team_id INT,
+    home_team_score INT,
+    away_team_score INT,
+    FOREIGN KEY (home_team_id) REFERENCES clubs(id),
+    FOREIGN KEY (away_team_id) REFERENCES clubs(id)
+);
